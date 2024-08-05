eb180a71037e794475624039240a752cd2fdfd93
Omar St <streete.omar@gmail.com>
2024-08-05 13:14:29 -0400

    Added a solver for quadratics

---
+++ quadratic.py
@@ -0,0 +1,31 @@
+#Solve all quadratic equations, returns both solutions as a tuple. Note: Sometimes the solutions are complex numbers.
+class Quadratic:
+    #stores the values in the eq.
+    def __init__(self, a, b, c=0):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.formula = f'{a}x^2 {b}x {c} = 0'
+        self.x = (0,0)
+
+    #The quadratic formula includes a section, sqrt(b^2 - 4ac), which is both added and subtracted, to return 2 solutions
+    def inner(self):
+        a, b, c = self.a, self.b, self.c
+        return (b**2 - 4*a*c)**(1/2)
+     
+    def calc(self, inner):
+        a, b = self.a, self.b
+        return (-b + inner)/(2*a)
+    
+    def quad(self):
+        a,b,c, calc,inner = self.a, self.b, self.c, self.calc, self.inner
+        p = self.calc(self.inner())
+        q = self.calc(-1*self.inner())
+        #Uses both the + and - of inner() to return 2 solutions
+        self.x = (p, q)
+        return (p, q)
+        
+#For example
+eg = Quadratic(3, 13, 12)
+print(eg.formula, '  equals  ', eg.quad())
+# -4/3 and -3
